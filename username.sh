ec7ca68a22b410383d057de1c2ec372334045d4a
Michael Fahy <fahy@chapman.edu>
2018-11-14 10:13:36 -0800

    Adds username.sh

---
+++ username.sh
@@ -0,0 +1,21 @@
+#! /bin/bash#! /bin/bash
+
+# username.sh
+
+echo "Enter a username: "
+
+read NAME
+
+while echo $NAME | egrep -v "^[a-z][_0-9a-z]{2,11}$" > /dev/null 2>&1
+
+do
+
+echo "You must enter a valid username!"
+
+echo "Enter a username: "
+
+read NAME
+
+done
+
+echo "Thank you"
